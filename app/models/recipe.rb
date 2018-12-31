c33e7f7203b2279505f2699396d2db05ce7bfde4
jonathaneyster <jonathaneyster@gmail.com>
2018-12-31 16:54:29 +0000

    added relation between user and recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: {minimum: 5, maxiumum: 500}
+  
+  belongs_to :user
+  validates :user_id, presence: true
 end
